52f241f684c8a79ecdb62779b945664e5e66864f
Nadav Shaoulian <70063788+nailev@users.noreply.github.com>
2022-03-31 17:29:58 +0300

    " all required without validation"

--- customers/Customer.py
+++ customers/Customer.py
@@ -2,18 +2,25 @@ from tokenize import Name
 from pydantic import BaseModel
 from customers.NameBoundary import NameBoundary
 
-class Customer():
+class Customer(BaseModel):
+    name:NameBoundary
+    email:str
+    password:str
+    birthdate:str
 
-    def __init__(self, name:NameBoundary, email, password) -> None:
-        self.name = name
-        self.email = email
-        self.password = password
 
 class CustomerBoundary(BaseModel):
     name:NameBoundary = None
     email:str = None
-    # def __init__(self, name = None, email = None , password=None) -> None:
-    #     self.name = name
-    #     self.email = email
-    #     self.password = password
+    birthdate:str = None
 
+    def make_cus_bound_from_cus(self, cus:Customer):
+        self.name = cus.name
+        self.email = cus.email
+        self.birthdate = cus.birthdate
+        return self
+
+class CustomerUpdater(BaseModel):
+    name:NameBoundary = None
+    password:str = None
+    birthdate:str = None
