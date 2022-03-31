11c11b3742c272d9fc9cf05be9ad16d1081abbd2
Nadav Shaoulian <70063788+nailev@users.noreply.github.com>
2022-03-31 19:16:13 +0300

    "all required except validation see TODO"

--- customers/Customer.py
+++ customers/Customer.py
@@ -7,20 +7,24 @@ class Customer(BaseModel):
     email:str
     password:str
     birthdate:str
+    roles:list
 
 
 class CustomerBoundary(BaseModel):
     name:NameBoundary = None
     email:str = None
     birthdate:str = None
+    roles:list = None
 
     def make_cus_bound_from_cus(self, cus:Customer):
         self.name = cus.name
         self.email = cus.email
         self.birthdate = cus.birthdate
+        self.roles = cus.roles
         return self
 
 class CustomerUpdater(BaseModel):
     name:NameBoundary = None
     password:str = None
     birthdate:str = None
+    roles:list = None
--- src/app.py
+++ src/app.py
@@ -8,7 +8,6 @@ from typing import Optional
 from fastapi_pagination import Page, add_pagination, paginate
 
 
-
 # from pydantic import BaseModel
 from customers.Customer import Customer, CustomerBoundary, CustomerUpdater
 from customers.NameBoundary import NameBoundary, FriendBoundary
@@ -17,10 +16,10 @@ SORT_OPTIONS = {'date':lambda x:datetime.datetime.strptime(x.birthdate, "%d-%m-%
                 'email':lambda x:x.email,'name':lambda x:x.name.first+x.name.last} 
 app = FastAPI()
 customers = {
-  "asd": Customer(name=NameBoundary(first='nadav',last='s'), email='asd2@gmail.com', password='123',birthdate='04-09-1993'),
-  "asd2": Customer(name=NameBoundary(first='lidor',last='amitay'), email='asd4@hotmail.com', password='123',birthdate='04-01-1992'),
-  "asd3": Customer(name=NameBoundary(first='noam',last='marko'), email='asd3@gmail.com', password='123',birthdate='01-08-1994'),
-  "asd4": Customer(name=NameBoundary(first='nissan',last='dalva'), email='asd@walla.co.il', password='123',birthdate='04-10-1993'),
+  "nadav@gmail.com": Customer(name=NameBoundary(first='nadav',last='s'), email='nadav@gmail.com', password='123',birthdate='04-09-1993',roles=["goldCustomer","platinumClub","primeService"]),
+  "lidor@hotmail.com": Customer(name=NameBoundary(first='lidor',last='amitay'), email='lidor@hotmail.com', password='123',birthdate='04-01-1992',roles=["goldCustomer","platinumClub","   "]),
+  "noam@gmail.com": Customer(name=NameBoundary(first='noam',last='marko'), email='noam@gmail.com', password='123',birthdate='01-08-1994',roles=["lowService"]),
+  "nissan@walla.co.il": Customer(name=NameBoundary(first='nissan',last='dalva'), email='nissan@walla.co.il', password='123',birthdate='04-10-1993',roles=["freeAcount"]),
 }
 
 friends = {}
@@ -47,17 +46,16 @@ async def login(email:str, password:str):
 
     raise HTTPException(status_code=404, detail="Customer not found") 
 
-
+#TODO: validations
 @app.post("/customers")
-async def createCustomer(fullname:NameBoundary, email:str, password:str,birthdate:str):
+async def createCustomer(fullname:NameBoundary, email:str, password:str,birthdate:str,roles:list):
     global customers
     if email in customers.keys(): 
         raise HTTPException(status_code=500, detail="Customer already exist")
-    customers[email] = Customer(name=fullname, email=email, password=password,birthdate=birthdate) 
-    # return CustomerBoundary().make_cus_bound_from_cus(customers[email])
-    return customers
+    customers[email] = Customer(name=fullname, email=email, password=password,birthdate=birthdate,rolse=roles) 
+    return CustomerBoundary().make_cus_bound_from_cus(customers[email])
 
-
+#TODO: validations 
 @app.put("/customers/{email}", response_model = CustomerBoundary)
 async def updateCostumer(email:str, customer:CustomerUpdater):
     global customers
@@ -74,6 +72,7 @@ async def updateCostumer(email:str, customer:CustomerUpdater):
 
 @app.put('/customers/{email}/friends')
 async def friends_connection(email:str, friend:FriendBoundary):
+    global friends
     if email in customers.keys() and friend.email in customers.keys():
         
         if friend.email not in friends.keys():
@@ -107,6 +106,8 @@ async def search_customer(sortBy=None, sortOrder=None, criteriaType=None,criteri
         cus_boundary_list = [CustomerBoundary().make_cus_bound_from_cus(c) for c in [customers[email] for email in customers.keys()] if c.birthdate.split('-')[-1] == criteriaValue]
     elif criteriaType=='byEmailDomain' and criteriaValue:
         cus_boundary_list = [CustomerBoundary().make_cus_bound_from_cus(c) for c in [customers[email] for email in customers.keys()] if c.email.split("@")[-1] == criteriaValue]
+    elif criteriaType=='byRole' and criteriaValue:
+        cus_boundary_list = [CustomerBoundary().make_cus_bound_from_cus(c) for c in [customers[email] for email in customers.keys()] if criteriaValue in c.roles]
     if sortBy:
         if sortBy in SORT_OPTIONS:
             return paginate(sorted(cus_boundary_list,key=SORT_OPTIONS[sortBy], reverse=True if sortOrder=='DESC' else False))
@@ -114,5 +115,20 @@ async def search_customer(sortBy=None, sortOrder=None, criteriaType=None,criteri
             HTTPException(status_code=400, detail="Invalid sort parameter") 
     return paginate(sorted(cus_boundary_list,key=SORT_OPTIONS['email']))    
 
+
+@app.get('/friends/')
+async def customer_friends_list():
+    return friends
+
+
+@app.get('/customers/{email}/friends/secondLevel',response_model=Page[CustomerBoundary])
+async def second_level_customer_friends_list(email:str):
+    if email in friends:
+        third = first = friends[email]
+        second = []
+        for e2 in first:
+            second.extend(friends[e2])
+        third.extend(second)
+        return paginate([CustomerBoundary().make_cus_bound_from_cus(c) for c in [customers[email] for email in set(third)]])
 
 add_pagination(app)
